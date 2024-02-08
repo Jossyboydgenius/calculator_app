bdc62cd069e21df9a4634a447022c87e9de1eabc
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 18:50:36 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,4 @@ class MyApp extends StatelessWidget {
     super.key,
   });
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(),
-    );
-  }
-}
+  

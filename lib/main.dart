8e5729a77bbd3ca077c0eb4e01921050879085e1
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 23:58:29 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
-import 'package:calculator_app/app.dart';
+// import 'package:calculator_app/app.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     const App(),
+//   );
+// }
+
 import 'package:flutter/material.dart';
+import 'package:calculator_app/app.dart';
+import 'package:calculator_app/calculator_screen.dart';
 
 void main() {
   runApp(
-    const App(),
+    const App(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => CalculatorScreen(),
+      },
+    ),
   );
 }
+

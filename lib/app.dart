c538768a95fe0f030f1bdb93ce29b854ec870a7e
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 19:47:02 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,20 @@ import 'package:calculator_app/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
  
+ class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +25,7 @@ import 'package:flutter/widgets.dart';
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: CalculatorScreen(),
+      home: CalculatorScreen (),
     );
   }
 

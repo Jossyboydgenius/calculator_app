12c21cfb09a3ce2dde17e969766ed8a530917251
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 21:20:45 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,21 @@
-// import 'package:calculator_app/screens/calculator_screen.dart';
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class App extends StatefulWidget {
-//   const App({
-//     super.key,
-//   });
+class App extends StatefulWidget {
+  const App({
+    super.key,
+  });
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
 
 //   @override
 //   Widget build(BuildContext context) {

e87faa0f4349bd6de5d04c33c4e8b71d16f08088
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 18:29:46 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),

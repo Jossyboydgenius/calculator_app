e1d4ce48c895819b554992659f00df2b779361d5
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 18:47:57 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold ),
     );
   }
 }

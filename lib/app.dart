62ad1980bf402b545a8281c20aa0b7ffc769e232
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 21:39:19 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,33 +1,15 @@
-import 'package:flutter/material.dart';
-class App extends StatefulWidget {
-  const App({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
-    );
-  }
-}
-
-
+// import 'package:flutter/material.dart';
 // class App extends StatefulWidget {
 //   const App({
-//     super.key,
-//   });
+//     Key? key,
+//   }) : super(key: key);
 
+//   @override
+//   _AppState createState() => _AppState();
+// }
+
+// class _AppState extends State<App> {
+//   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
 //       debugShowCheckedModeBanner: false,
@@ -39,3 +21,21 @@ class _AppState extends State<App> {
 //     );
 //   }
 // }
+
+
+class App extends StatefulWidget {
+  const App({
+    super.key,
+  });
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}

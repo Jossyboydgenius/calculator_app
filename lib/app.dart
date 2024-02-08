a074559f2d064595b9385e999964168de67280af
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 22:46:37 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -16,3 +16,28 @@
 //     );
 //   }
 // }
+
+import 'package:flutter/material.dart';
+
+class AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  AppState createState() => AppState();
+}

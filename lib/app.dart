362221ed9b32ee8d13dd582c8f029bbaefcbac7b
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 20:55:26 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,28 +1,47 @@
-import 'package:calculator_app/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatefulWidget {
-  const App({
-    super.key,
-  });
+class CalcultorSreen extends StatefulWidget {
+  const CalcultorSreen({super.key});
 
+  @override
+  State<CalcultorSreen> createState() => _CalcultorSreenState();
+}
+
+class _CalcultorSreenState extends State<CalcultorSreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      home: const CalculatorScreen(),
-    );
-  }
-
-  @override
-  // ignore: no_logic_in_create_state
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    return const Placeholder();
   }
 }
+
+
+
+
+// import 'package:calculator_app/screens/calculator_screen.dart';
+// import 'package:flutter/material.dart';
+
+// class App extends StatefulWidget {
+//   const App({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Calculator App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         useMaterial3: true,
+//       ),
+//       home: const CalculatorScreen(),
+//     );
+//   }
+
+//   @override
+//   // ignore: no_logic_in_create_state
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     throw UnimplementedError();
+//   }
+// }

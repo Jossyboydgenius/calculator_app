579398a859f6ffac13b66c5e0002d91f9ad15d92
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 18:48:30 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold ),
+      home: Scaffold(),
     );
   }
 }

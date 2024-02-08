d45255942abb07aa711e63100e2d8e3b2bb9658c
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 18:59:23 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
+import 'package:calculator_app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const App(),
+  );
 }
-
-class App extends StatelessWidget {
-  const App({
-    super.key,
-  });
-}
- @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(),
-    );
-  }

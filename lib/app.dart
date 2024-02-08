16f158a811843d2096e9d366b0c3fcd38ad36df9
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 18:52:03 +0000

    Commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
+ 

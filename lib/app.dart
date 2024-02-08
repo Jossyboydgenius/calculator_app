64a0f830ada8263444121ae8fa146fdff5ef917d
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-08 21:24:46 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -16,24 +16,3 @@ class App extends StatefulWidget {
     );
   }
 }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Calculator App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         useMaterial3: true,
-//       ),
-//       home: const CalculatorScreen(),
-//     );
-//   }
-
-//   @override
-//   // ignore: no_logic_in_create_state
-//   State<StatefulWidget> createState() {
-//     // TODO: implement createState
-//     throw UnimplementedError();
-//   }
-// }

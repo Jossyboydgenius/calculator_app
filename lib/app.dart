cf3cbcfd57600ea36a2b23fcbc91ad3b89ae3e52
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-09 15:05:52 +0000

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,31 +1,10 @@
-import 'package:flutter/material.dart';
-
-class App extends StatefulWidget {
-  const App({
-    super.key,
-  });
-
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
-    );
-  }
-}
-
-
-
 // import 'package:flutter/material.dart';
-// import 'package:calculator_app/screens/calculator_screen.dart';
 
-// class App extends StatelessWidget {
-//   const App({Key? key}) : super(key: key);
+// class App extends StatefulWidget {
+//   const App({
+//     super.key,
+//   });
 
-//   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
 //       debugShowCheckedModeBanner: false,
@@ -33,7 +12,28 @@ class App extends StatefulWidget {
 //       theme: ThemeData(
 //         primarySwatch: Colors.blue,
 //       ),
-//       home: const CalculatorScreen(),
+//       home: const Scaffold(),
 //     );
 //   }
 // }
+
+
+
+import 'package:flutter/material.dart';
+import 'package:calculator_app/screens/calculator_screen.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}

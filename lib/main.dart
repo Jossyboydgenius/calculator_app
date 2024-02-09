8d5d0403764baab32cb3bd5135b700787526ffa3
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-09 12:31:59 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@
 //   );
 // }
 
-
-
 import 'package:flutter/material.dart';
 import 'package:calculator_app/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const App(),
+  );
 }

bb945959d66d55441ade250f68dbe917f2eb92d6
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-09 16:34:29 +0000

    Commit

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -110,18 +110,3 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
-class CustomButton extends StatefulWidget {
-  const CustomButton({
-    super.key,
-  });
-
-  @override
-  State<CustomButton> createState() => _CustomButtonState();
-}
-
-class _CustomButtonState extends State<CustomButton> {
-  @override
-  Widget build(BuildContext context) {
-    
-  }
-}

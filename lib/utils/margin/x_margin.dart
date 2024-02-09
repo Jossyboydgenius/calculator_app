580ece564807a6dc8fb16a014bc3094435daeedc
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-09 23:32:23 +0000

    Commit

--- lib/utils/margin/x_margin.dart
+++ lib/utils/margin/x_margin.dart
@@ -9,6 +9,8 @@ class XMargin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: value,
+    );
   }
 }

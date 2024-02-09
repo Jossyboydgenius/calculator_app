3e7a0a3287c20b984a7bf9aba7c0cde0ec06685b
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-09 23:29:48 +0000

    Commit

---
+++ lib/utils/margin/x_margin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class XMargin extends StatelessWidget {
+  const XMargin({super.key});
+  final double value;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

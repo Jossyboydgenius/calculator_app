3130fa3f91add3f9e19f67834266cfcf5019c5b7
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 05:04:36 +0000

    Commit

--- lib/utils/calculator_functions.dart
+++ lib/utils/calculator_functions.dart
@@ -1,42 +1,42 @@
-import 'package:math_expressions/math_expressions.dart';
+// import 'package:math_expressions/math_expressions.dart';
 
-class CalculatorFunctions {
-  // Defining the expression and the result variable
-  String expression = '';
-  String result = '';
-  // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
-  void performButtonAction(String buttonText) {
-    if (buttonText == 'C') {
-      expression = '';
-      result = '';
-    } else if (buttonText == '⌫') {
-      if (expression.isEmpty) {
-        return;
-      }
-      expression = expression.substring(0, expression.length - 1);
-    } else if (buttonText == '=') {
-      try {
-        Parser parser = Parser();
-        Expression exp = parser.parse(expression);
-        ContextModel context = ContextModel();
-        final evaluatedExpression = exp.evaluate(
-          EvaluationType.REAL,
-          context,
-        );
-        result =
-            '${isInterger(evaluatedExpression) ? (evaluatedExpression as num).ceil() : '$evaluatedExpression'}';
-        expression = result;
-      } catch (e) {
-        result = 'Error $e';
-      }
-    } else {
-      expression += buttonText;
-    }
-  }
+// class CalculatorFunctions {
+//   // Defining the expression and the result variable
+//   String expression = '';
+//   String result = '';
+//   // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
+//   void performButtonAction(String buttonText) {
+//     if (buttonText == 'C') {
+//       expression = '';
+//       result = '';
+//     } else if (buttonText == '⌫') {
+//       if (expression.isEmpty) {
+//         return;
+//       }
+//       expression = expression.substring(0, expression.length - 1);
+//     } else if (buttonText == '=') {
+//       try {
+//         Parser parser = Parser();
+//         Expression exp = parser.parse(expression);
+//         ContextModel context = ContextModel();
+//         final evaluatedExpression = exp.evaluate(
+//           EvaluationType.REAL,
+//           context,
+//         );
+//         result =
+//             '${isInterger(evaluatedExpression) ? (evaluatedExpression as num).ceil() : '$evaluatedExpression'}';
+//         expression = result;
+//       } catch (e) {
+//         result = 'Error $e';
+//       }
+//     } else {
+//       expression += buttonText;
+//     }
+//   }
 
-  // Creating a function that determines if a number is a interget or not
-  bool isInterger(num value) => value is int || value == value.roundToDouble();
-}
+//   // Creating a function that determines if a number is a interget or not
+//   bool isInterger(num value) => value is int || value == value.roundToDouble();
+// }
 
 import 'package:math_expressions/math_expressions.dart';
 

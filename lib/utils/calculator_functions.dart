a9f2444925b7dd7751f1419b1e48689d3719b128
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 01:42:08 +0000

    Commits

--- lib/utils/calculator_functions.dart
+++ lib/utils/calculator_functions.dart
@@ -1,1 +1,5 @@
-class CalculatorFunctions {}
+class CalculatorFunctions {
+  // Defining the expression and the result variable
+  // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
+  // 
+}

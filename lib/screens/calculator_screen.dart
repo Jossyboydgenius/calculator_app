2eb6d465be7c1cb95f7299ccb9da2fc0dce50847
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 00:12:34 +0000

    Commit

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -169,7 +169,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                   textColor: Colors.white,
                 ),
                 CustomButton(
-                  buttonText: '52',
+                  buttonText: '2',
                   buttonColor: Colors.white12,
                   onPressed: () {},
                   textColor: Colors.white,

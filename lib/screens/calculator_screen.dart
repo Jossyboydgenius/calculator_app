99e02a7d71df04962fa1da0bd1ba8f4d7b6418bd
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 00:04:54 +0000

    Commit

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -158,6 +158,36 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                 ),
               ],
             ),
+             const YMargin(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomButton(
+                  buttonText: '4',
+                  buttonColor: Colors.white12,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+                CustomButton(
+                  buttonText: '5',
+                  buttonColor: Colors.white12,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+                CustomButton(
+                  buttonText: '6',
+                  buttonColor: Colors.white12,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+                CustomButton(
+                  buttonText: '-',
+                  buttonColor: Colors.orange,
+                  onPressed: () {},
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
           ],
         ),
       ),

92f3bec942f2dc663b824c902c8e63f2b65dd0ea
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 01:52:07 +0000

    Commit

--- lib/utils/calculator_functions.dart
+++ lib/utils/calculator_functions.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 class CalculatorFunctions {
   // Defining the expression and the result variable
   String expression = '';
   String result = '';
   // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
+  Void 
   // Creating a function that determines if a number is a interget or not
   bool isInterger(num value) => value is int || value == value.roundToDouble();
 }

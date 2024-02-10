f5535ec3b2a89ea4f8d613b2640cbd6fdcea5a2d
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 02:54:43 +0000

    Commit

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -84,7 +84,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                     buttonColor: Colors.grey,
                     onPressed: () {
                       setState(() {
-                      calculatorFunctions.performButtonAction('8');
+                      calculatorFunctions.performButtonAction('C');
                       });
                       },
                     textColor: Colors.white,

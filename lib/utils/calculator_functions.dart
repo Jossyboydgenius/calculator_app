cf15c42619bb7e64f89b3372e3a9104fe42adcc5
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 05:04:11 +0000

    Commit

--- lib/utils/calculator_functions.dart
+++ lib/utils/calculator_functions.dart
@@ -1,67 +1,50 @@
-// import 'package:math_expressions/math_expressions.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-// class CalculatorFunctions {
-//   // Defining the expression and the result variable
-//   String expression = '';
-//   String result = '';
-//   // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
-//   void performButtonAction(String buttonText) {
-//     if (buttonText == 'C') {
-//       expression = '';
-//       result = '';
-//     } else if (buttonText == '⌫') {
-//       if (expression.isEmpty) {
-//         return;
-//       }
-//       expression = expression.substring(0, expression.length - 1);
-//     } else if (buttonText == '=') {
-//       try {
-//         Parser parser = Parser();
-//         Expression exp = parser.parse(expression);
-//         ContextModel context = ContextModel();
-//         final evaluatedExpression = exp.evaluate(
-//           EvaluationType.REAL,
-//           context,
-//         );
-//         result =
-//             '${isInterger(evaluatedExpression) ? (evaluatedExpression as num).ceil() : '$evaluatedExpression'}';
-//         expression = result;
-//       } catch (e) {
-//         result = 'Error $e';
-//       }
-//     } else {
-//       expression += buttonText;
-//     }
-//   }
-
-//   // Creating a function that determines if a number is a interget or not
-//   bool isInterger(num value) => value is int || value == value.roundToDouble();
-// }
-
-// ... Existing code ...
-
-  // New method for handling percentage
-  void handlePercentage() {
-    if (expression.isEmpty) {
-      return;
-    }
-    try {
-      Parser parser = Parser();
-      Expression exp = parser.parse(expression);
-      ContextModel context = ContextModel();
-      final evaluatedExpression = exp.evaluate(
-        EvaluationType.REAL,
-        context,
-      );
-      final percentageResult = evaluatedExpression / 100;
-      result = '$percentageResult';
-      expression = result;
-    } catch (e) {
-      result = 'Error $e';
+class CalculatorFunctions {
+  // Defining the expression and the result variable
+  String expression = '';
+  String result = '';
+  // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
+  void performButtonAction(String buttonText) {
+    if (buttonText == 'C') {
+      expression = '';
+      result = '';
+    } else if (buttonText == '⌫') {
+      if (expression.isEmpty) {
+        return;
+      }
+      expression = expression.substring(0, expression.length - 1);
+    } else if (buttonText == '=') {
+      try {
+        Parser parser = Parser();
+        Expression exp = parser.parse(expression);
+        ContextModel context = ContextModel();
+        final evaluatedExpression = exp.evaluate(
+          EvaluationType.REAL,
+          context,
+        );
+        result =
+            '${isInterger(evaluatedExpression) ? (evaluatedExpression as num).ceil() : '$evaluatedExpression'}';
+        expression = result;
+      } catch (e) {
+        result = 'Error $e';
+      }
+    } else {
+      expression += buttonText;
     }
   }
 
-  // Modified method to handle the new '%' button
+  // Creating a function that determines if a number is a interget or not
+  bool isInterger(num value) => value is int || value == value.roundToDouble();
+}
+
+import 'package:math_expressions/math_expressions.dart';
+
+class CalculatorFunctions {
+  // Defining the expression and the result variable
+  String expression = '';
+  String result = '';
+  // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
   void performButtonAction(String buttonText) {
     if (buttonText == 'C') {
       expression = '';
@@ -93,4 +76,27 @@
     }
   }
 
-// ... Existing code ...
+  // New method for handling percentage
+  void handlePercentage() {
+    if (expression.isEmpty) {
+      return;
+    }
+    try {
+      Parser parser = Parser();
+      Expression exp = parser.parse(expression);
+      ContextModel context = ContextModel();
+      final evaluatedExpression = exp.evaluate(
+        EvaluationType.REAL,
+        context,
+      );
+      final percentageResult = evaluatedExpression / 100;
+      result = '$percentageResult';
+      expression = result;
+    } catch (e) {
+      result = 'Error $e';
+    }
+  }
+
+  // Creating a function that determines if a number is an integer or not
+  bool isInterger(num value) => value is int || value == value.roundToDouble();
+}

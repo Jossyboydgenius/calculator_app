5754471a0c21ad529197ca0d761035e770494201
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 01:03:56 +0000

    Commit

---
+++ lib/Widgets/custom_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.buttonText,
+    required this.buttonColor,
+    required this.textColor,
+    required this.onPressed,
+  });
+  final String buttonText;
+  final Color buttonColor;
+  final Color textColor;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.height * 0.08,
+      height: size.height * 0.08,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: buttonColor,
+          shape: const CircleBorder(),
+        ),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}

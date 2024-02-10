69b8d632799f8bf51c23cc513b45c626ec7ed321
jossyboydgenius <jossyboydgenius@gmail.com>
2024-02-10 01:45:07 +0000

    Commit

--- lib/utils/calculator_functions.dart
+++ lib/utils/calculator_functions.dart
@@ -1,5 +1,7 @@
 class CalculatorFunctions {
   // Defining the expression and the result variable
+  String expression = '';
+  String result = '';
   // Creating a function that helps in handling whatever button is clicked and also evaluate the expression
-  // 
+  // Creating a function that determines if a number is a interget or not
 }
